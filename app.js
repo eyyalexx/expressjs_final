86462e4b4a3fb11f618c8eaac814c8adb16f9fe9
Kevin <k.nguyenphams@gmail.com>
2017-11-17 20:30:03 -0500

    minor css stuff
    also added my profile

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.post('/page', function(request, response){
 			var contributors = [
         			{ name: 'Alex Grigorev', picture: 'pictures/No_Picture.jpg' },
         			{ name: 'Taranpreet Saini', picture: 'pictures/No_Picture.jpg' },
-        			{ name: 'Kevin Nguyen', picture: 'pictures/No_Picture.jpg' },
+        			{ name: 'Kevin Nguyen', picture: 'pictures/Kevin.jpg' },
 				{ name: 'Dawson Brown', picture: 'pictures/Dawson.jpg'}
     			];
 			response.render('credits', {contributors: contributors});
